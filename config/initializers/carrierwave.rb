e6ab8f7d28abe7bdfe5466bca4b0b56ec61e3311
maungmoemyint <maungmoe.myint@aya.yale.edu>
2021-11-05 15:23:58 +0100

    bundle exec figaro run and modified carrierwave rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,4 +8,8 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],
   }
   config.fog_directory  = ENV["AWS_BUCKET"]
+  config.fog_public    = false
+  config.cache_dir     = "#{Rails.root}/tmp/uploads"         # To let CarrierWave work on Heroku
+  config.storage       = :fog
+
 end
