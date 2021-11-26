b263e8f42c56e1c5d2c1e1330c861c21a9f33b53
maungmoemyint <maungmoe.myint@aya.yale.edu>
2021-11-26 12:56:21 +0100

    Tip model search scope and validates modified to get same results of search regardless of upper or lower cases

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -5,17 +5,17 @@ class Tip < ApplicationRecord
   has_and_belongs_to_many :users
 
   validates :title, presence: true,
-                    length: { maximum: 100 }
-  validates :body,  presence: true
+                    length: { maximum: 100 },
+                    uniqueness: {case_sensitive: false}
+  validates :body,  presence: true,
+                    uniqueness: {case_sensitive: false}
 
   scope :most_recent,          -> { order(updated_at: :desc).limit(4) }
-  scope :title_contains,       ->(item) { where('title LIKE ?', "%#{item}%") }
-  scope :body_contains,        ->(item) { where('body LIKE ?', "%#{item}%") }
-  scope :search,               ->(search_term) { title_contains((search_term).upcase).
-                                                 or(title_contains((search_term).downcase)).
-                                                 or(body_contains((search_term).downcase)).
-                                                 or(body_contains((search_term).upcase))
-                                               }
+  scope :title_contains,       ->(item) { where('title LIKE ?', "%#{item}%".downcase ) }
+
+  scope :body_contains,        ->(item) { where('body LIKE ?', "%#{item}%".downcase ) }
+
+  scope :search,               ->(search_term) { title_contains(search_term).or(body_contains(search_term)) }
 
   paginates_per 8
 
