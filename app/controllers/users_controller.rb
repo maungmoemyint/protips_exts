125f420070414bc3ec14c3e0578b27c71d1dbb89
maungmoemyint <maungmoe.myint@aya.yale.edu>
2021-11-26 09:50:59 +0100

    alphabectical order or methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-  end
+
 
   def create
     @user = User.new(user_params)
@@ -29,6 +28,9 @@ class UsersController < ApplicationController
         format.html { render :new }
       end
     end
+  end
+
+  def edit
   end
 
   def update
