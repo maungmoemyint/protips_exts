bebd1d84b9768baa68647771c849bddb483609f7
maungmoemyint <maungmoe.myint@aya.yale.edu>
2021-11-29 15:38:39 +0100

    security issue resolved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
+
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :ensure_authenticated, :current_user
-  before_action :authorize_to_edit_user
+
+  #before_action :ensure_admin, only: [:index, :show, :edit, :update, :destroy]
+  before_action :ensure_authenticated, only: [:index, :edit, :update, :destroy]
+
+  #before_action :authorize_to_edit_user, only: [:index]
 
   def index
-    @users = User.all.page(params[:page])
+
+    user = User.find(session[:user_id])
+    if(user.role == 'admin')
+      @users = User.all.page(params[:page])
+    else
+      redirect_to(account_path)
+    end
+
   end
 
   def show
@@ -33,20 +44,29 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find params[:id]
   end
 
   def update
 
-    respond_to do |format|
-      if @user.update(edit_user_params)
-        # In this format call, the flash message is being passed directly to
-        # redirect_to().  It's a caonvenient way of setting a flash notice or
-        # alert without referencing the flash Hash explicitly.
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-      else
-        format.html { render :edit }
+    #user = User.find(session[:user_id])
+    #if(user.id = @user.id)
+
+      respond_to do |format|
+        if @user.update(edit_user_params)
+          # In this format call, the flash message is being passed directly to
+          # redirect_to().  It's a caonvenient way of setting a flash notice or
+          # alert without referencing the flash Hash explicitly.
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
       end
-    end
+
+    # else
+    #   redirect_to account_path
+    # end
+
   end
 
   def destroy
@@ -63,7 +83,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :name, :avatar, :password, :role)
+      params.require(:user).permit(:email, :name, :avatar, :password)
     end
 
     def edit_user_params
@@ -73,4 +93,13 @@ class UsersController < ApplicationController
     def authorize_to_edit_user
       redirect_to(account_path) unless(can_edit?(@user))
     end
+
+    def ensure_admin
+      if(current_user.role == 'admin')
+        return
+      end
+
+      redirect_to account_path
+    end
+
 end
